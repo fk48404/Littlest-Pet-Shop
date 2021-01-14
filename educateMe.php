ac965cae0fe2ed25e5755e8188e40d45998b68fa
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-01-14 23:50:16 +0100

    Update educateMe.php

--- educateMe.php
+++ educateMe.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Educate Me</title>
 	<link rel="stylesheet" type="text/css" href="css/educateMe.css">
+		<link rel="stylesheet" type="text/css" href="mobile/educateMe.css">
 </head>
 <body>
 <!--HEADER-->
