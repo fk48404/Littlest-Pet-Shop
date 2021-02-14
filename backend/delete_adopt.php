3af54449cea0e2fddeb1e836ebaaeedb3d3e7318
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-02-14 18:36:08 +0100

    Create delete_adopt.php

---
+++ backend/delete_adopt.php
@@ -0,0 +1,13 @@
+<?
+    require '../dbconnect.php';
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+    }
+
+    $query = "DELETE FROM adopto WHERE id = :id";
+    $query = $pdo->prepare($query);
+
+    $query->execute(['id' => $id]);
+?>
+<? header("Location: ../dashboard.php");?>
