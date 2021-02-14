d2948dfdde76d5e76de1990fb998ce128fea17f0
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-02-14 12:56:33 +0100

    Update loginregister.php

--- loginregister.php
+++ loginregister.php
@@ -1,19 +1,18 @@
+<? include 'dbconnect.php'?>
 <html>
 <head>
 	<title>Login/Register</title>
 	<link rel="stylesheet" type="text/css" href="css/loginregister.css">
 	<link rel="stylesheet" type="text/css" href="mobile/loginregister.css">
-
 </head>
 <body>
 <!--HEADER-->
 	<?php include 'include/header.php'; ?>
 <br></br>
-
 	<div class="pjesaContact">
 	<h1>Per te na kontaktuar fillimisht duhet te ndjekni proceduen me poshte: </h1>
 	<p>( Ju lutem regjistrohuni ne rast qe nuk keni nje llogari ne faqen tone, nuk do te zhgenjeheni  &#9787;) </p>
-
+<!--Fillimi i formes-->
 		<div class="form-box">
 			<div class="button-box">
 				<div id="btn"></div>
@@ -25,22 +24,24 @@
 				<img src="images/c.png">
 				<img src="images/b.png">
 			</div>
-			<form action="loginregister.php" method="POST" id="login" class="input-group" onsubmit="return loginValidate()">
-				<input type="text" id="email" class="input-field" placeholder="Email">
-				<input type="password" id="pw" class="input-field" placeholder="Fjalekalimi"></br>
+<!---LOGIN--->
+			<form action="backend/login.php" method="POST" id="login" class="input-group" onsubmit="return loginValidate()">
+				<input type="text" name="email" id="email" class="input-field" placeholder="Email">
+				<input type="password" name="password" id="pw" class="input-field" placeholder="Fjalekalimi"></br>
 				<button type="submit" name= "submit" class="submit-btn" onclick="validimi(0)"> Log in</button>
 			</form>
-<? include 'backendphp/insertimeReg.php' ?>
-			<form action="loginregister.php" method="POST" id="register" name="reg" class="input-group" onsubmit="return registerValidate()">
-				<input type="text" id="emri" class="input-field" placeholder="Emri">
-				<input type="text" id="mbiemri" class="input-field"  placeholder="Mbiemri">
-				<input type="text" id="mail" class="input-field"  placeholder="Email">
+<!---REGISTER--->
+			<form action="backend/register.php" method="POST" id="register" name="reg" class="input-group" onsubmit="return registerValidate()">
+				<input type="text" id="emri" name="emri" class="input-field" placeholder="Emri">
+				<input type="text" id="mbiemri" name="mbiemri" class="input-field"  placeholder="Mbiemri">
+				<input type="text" id="mail" name="mail" class="input-field"  placeholder="Email">
 				<input type="password" id="psw" class="input-field" name="password" placeholder="Fjalekalimi">
-				<input type="password"  class="input-field" name="password2" placeholder="Konfirmo Fjalekalimin"></br>
+				<input type="password" class="input-field" name="password2" placeholder="Konfirmo Fjalekalimin"></br>
 				<button type="submit" name= "submit2" class="submit-btn" onclick="return validimi(1)">Register</button>
 			</form>
 		</div>
 	</div>
+<!--Fundi i formes-->
 	<?php include 'include/footer.php'; ?>	
 	<script src="js/loginregister.js"></script>
 </body>
